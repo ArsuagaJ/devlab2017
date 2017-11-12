aa4c8840a424abec0152f7dff7f752dcfe2ba726
Mephisto <Mephisto@DESKTOP-NSO3DOM>
2017-11-12 14:59:38 -0300

    se modifico pantallas de admin y representante, ademas de agregarse la pantalla admin_modificar_usuario

--- Gaseosas_Arenba/public_html/php/conexion.php
+++ Gaseosas_Arenba/public_html/php/conexion.php
@@ -2,29 +2,17 @@
 $servername = "localhost";$username = "root";$password = "";
 $dbname = "gaseosaarenba";
 $idUsuario= "1";
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username,$password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn;
 
 /*lkjajsdjfladjkfjald*/
 
-/*try {
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username,
-$password);
+try {
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username,$password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$stmt = $conn->prepare("SELECT id_usuario, rol, nombre, apellido FROM usuario WHERE
-id_usuario=1");
-//$stmt->bindParam(':localidad', $firstname);
-$stmt->execute();
-
-$resultado = $stmt->fetchAll();
-    foreach ($resultado as $row) {
-        echo $row["apellido"];
-    }
-
 }
 catch(PDOException $e) {
 echo "Error: " . $e->getMessage();
-}*/
+}
 //$conn = null;
 
 ?>
