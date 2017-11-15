c705579a42673ce40fc9c72b79142fe5a67bb144
Win7 <Win7@Win7-PC>
2017-11-15 20:35:07 -0300

    agregue funcionalidad a registro cliente

--- Gaseosas_Arenba/public_html/js/usuario_registro.js
+++ Gaseosas_Arenba/public_html/js/usuario_registro.js
@@ -37,12 +37,15 @@ function validarTelefono(){
 	if (texto==""){
 			$('#pTelefono').text('debe ingresar el telefono');
 			event.preventDefault();
+                        return false;
 		}
 		else if(texto.length==8 || texto.length==10) { 
-			$('#pTelefono').text(' '); 	
+			$('#pTelefono').text(' ');
+                        return true;
 		} else {
 			$('#pTelefono').text('telefono invalido');
 			event.preventDefa
+                        return false;
 		}
 }
 
@@ -53,16 +56,20 @@ function validarNombre(){
 		if (texto==""){
 			$('#pNombre').text('debe ingresar el nombre');
 			event.preventDefault();
+                        return false;
 		}
 		else if(aux.length != 1) { 
 			$('#pNombre').text('el nombre debe tener una palabra ');
+                        event.preventDefault();
+                        return false;
 		}
 		else if(reg.test(texto)) { 
 			$('#pNombre').text(' ');
-			
+			return true;
 		} else {
 			$('#pNombre').text('nombre invalido');
 			event.preventDefault();
+                        return false;
 	}	
 }
 
@@ -72,13 +79,15 @@ function validarApellido(){
 		if (texto==""){
 			$('#pApellido').text('debe ingresar el apellido');
 			event.preventDefault();
+                        return false;
 		}
 		else if(reg.test(texto)) { 
 			$('#pApellido').text(' ');
-			
+                        return true;			
 		} else {
 			$('#pApellido').text('apellido invalido');
 			event.preventDefault();
+                        return false;
 	}	
 }
 
@@ -88,13 +97,15 @@ function validarLocalidad(){
 		if (texto==""){
 			$('#pLocalidad').text('debe ingresar la localidad');
 			event.preventDefault();
+                        return false;
 		}
 		else if(reg.test(texto)) { 
 			$('#pLocalidad').text(' ');
-			
+			return true;
 		} else {
 			$('#pLocalidad').text('localidad invalida');
 			event.preventDefault();
+                        return false;
 	}	
 }
 
@@ -104,13 +115,15 @@ function validarDirreccion(){
 		if (texto==""){
 			$('#pDireccion').text('debe ingresar la direccion');
 			event.preventDefault();
+                        return false;
 		}
 		else if(reg.test(texto)) { 
 			$('#pDireccion').text(' ');
-			
+			return true;
 		} else {
 			$('#pDireccion').text('direccion invalida');
 			event.preventDefault();
+                        return false;
 	}	
 }
 
@@ -120,12 +133,15 @@ function validarEmail(){
 		if (texto==""){
 			$('#pEmail').text('debe ingresar el e-mail');
 			event.preventDefault();
+                        return false;
 		}
 		else if(reg.test(texto)) { 
-			$('#pEmail').text(' '); 	
+			$('#pEmail').text(' ');
+                        return true;
 		} else {
 			$('#pEmail').text('e-mail invalido');
 			event.preventDefault();
+                        return false;
 	}	
 }
 
@@ -134,11 +150,15 @@ function validarDni(){
         if (texto==""){
                 $('#pDni').text('debe ingresar el DNI');
                 event.preventDefault();
+                return false;
         }
         else if(texto.length == 8 || texto.length == 10) { 
                 $('#pDni').text(' ');
+                return true;
         }else {
                 $('#pDni').text('DNI invalido');
+                event.preventDefault();
+                return false;
         }	
 }
 
@@ -148,26 +168,96 @@ function validarProvincia(){
 		if (texto==""){
 			$('#pProvincia').text('debe ingresar una provincia');
 			event.preventDefault();
+                        return false;
 		}
 		else if(reg.test(texto)) { 
 			$('#pProvincia').text(' ');
-			
+			return true;
 		} else {
 			$('#pProvincia').text('provincia invalido');
 			event.preventDefault();
+                        return false;
 	}	
 }
 
 $(document).ready(function(){
 	$('#registro').click(function() {
 		//Utilizamos una expresion regular 
-		validarEmail();
-		validarTelefono();
-		validarNombre();
-		validarApellido();
-		validarLocalidad();
-		validarDirreccion();
-                validarDni();
-                validarProvincia();
+		var var1 = validarEmail();
+		var var2 = validarTelefono();
+		var var3 = validarNombre();
+		var var4 = validarApellido();
+		var var5 = validarLocalidad();
+		var var6 = validarDirreccion();
+                var var7 = validarDni();
+                var var8 = validarProvincia();
+                
+                if (var1 === true & var2 === true & var3 === true & var4 === true & var5 === true & var6 === true & var7 === true & var8 === true){
+                    
+                    var formData = new FormData($("#js-upload-form")[0]);
+                    var ruta = "../php/cliente_registro.php"; // lo enviamos al php para que lo suba a la carpeta "fotos"
+                    $.ajax({
+                        url: ruta,
+                        type: "POST",
+                        data: formData,
+                        contentType: false,
+                        processData: false,
+                        beforeSend: function insertar() { // todavia no entiendo por que llamamos a la funcion "insertar()" que creo que deberia ser la del php, pero bueno...
+                            $("#pMensaje").text("Procesando, espere por favor...");
+
+                        }/*,
+                        success: function(response){
+                            $("#pMensaje").text(response);
+                        }*/
+                    }).done(function(response){ //recuperamos el valor de la ruta de destino '../fotos/nombre_archivo
+                        console.log(response); //lo grabamos en la consola
+                        //$("#pMensaje").text(response[1]);
+
+                        // si se subio la imagen, tomamos los valores de los campos
+                        var nombre = $("#nombre").val();
+                        var apellido = $("#apellido").val();
+                        var dni = $("#dni").val();
+                        var telefono = $("#telefono").val();
+                        var email = $("#email").val();
+                        var provincia = $("#provincia").val();
+                        var localidad = $("#localidad").val();
+                        var direccion = $("#direccion").val();
+                        var idUsuario = $("#idUsuario").val();
+                        var idPassword = $("#idPassword").val();
+                        var idPassword1 = $("#idPassword1").val();
+                        
+
+                        // los siguientes valores son los que le pasamos al php con ajax que luego los recuperara con el nombre descriptivo
+                        // que le hayamos puesto.. en este caso
+                        // "nombre", "descrip","punt","img","estado"
+                        var parametros = {
+                            "nombre" : nombre,
+                            "descrip" : descrip,
+                            "punt" : puntos,
+                            "img" : rutaImagen,
+                            "estado" : estado
+                        };
+                        // generamos un ajax nuevo con los valores de los campos
+                        $.ajax({
+                            data:  parametros, // los datos que van a ser recuperados desde el php
+                            url:   '../php/insertProductos.php', // llamamos al php para insertar los datos en este caso con los parametros que le pasemos
+                            type:  'post',
+                            success:  function (response) {
+                                console.log(response);
+                                $("#pMensaje").text(response);    
+                            }
+                        }).done(function(respuesta){
+                            $("#pMensaje").text("OK, se agregado correctamente el usuario");//fijarme si esta
+                        });
+                      });
+                    
+                    
+                    
+                    
+                                        
+                }else{
+                    alert("nonoononono");
+                }
+
 	}); 
 });
---
+++ Gaseosas_Arenba/public_html/php/cliente.php
@@ -0,0 +1,194 @@
+<?php
+
+    class Producto{
+        private $idProducto;
+        private $strNombreProducto;
+        private $strDescripcionProducto;
+        private $strFoto;
+        private $intPuntosCanje;
+        private $boolEstado;
+        private $conn;
+
+        function __construct(){
+            $this->idProducto = "";
+            $this->strNombreProducto = "";
+            $this->strDescripcionProducto = "";
+            $this->strFoto = "";
+            $this->intPuntosCanje = 0;
+            $this->boolEstado = false;
+        }
+     
+        function getIdProducto() {
+            return $this->idProducto;
+        }
+
+        function getStrNombreProducto() {
+            return $this->strNombreProducto;
+        }
+
+        function getStrDescripcionProducto() {
+            return $this->strDescripcionProducto;
+        }
+
+        function getStrFoto() {
+            return $this->strFoto;
+        }
+
+        function getIntPuntosCanje() {
+            return $this->intPuntosCanje;
+        }
+
+        function getBoolEstado() {
+            return $this->boolEstado;
+        }
+
+        function setIdProducto($idProducto) {
+            $this->idProducto = $idProducto;
+        }
+
+        function setStrNombreProducto($strNombreProducto) {
+            $this->strNombreProducto = $strNombreProducto;
+        }
+
+        function setStrDescripcionProducto($strDescripcionProducto) {
+            $this->strDescripcionProducto = $strDescripcionProducto;
+        }
+
+        function setStrFoto($strFoto) {
+            $this->strFoto = $strFoto;
+        }
+
+        function setIntPuntosCanje($intPuntosCanje) {
+            $this->intPuntosCanje = $intPuntosCanje;
+        }
+
+        function setBoolEstado($boolEstado) {
+            $this->boolEstado = $boolEstado;
+        }
+        
+        /* estas funciones por el momento no funcan
+        function conectarBD(){
+            return include('./conexion.php');
+        }
+        
+        function cerrarConexionBD(){
+            include('./desconexion.php');
+        }*/
+        
+        function getProductoByID($id_producto){
+            include './conexion.php';
+
+            $stmt = $conn->prepare("SELECT id_producto, nombre, descripcion, foto, puntos, estado FROM producto WHERE id_producto=:id_producto");
+            $stmt->bindParam(':id_producto', $id_producto);
+            
+            $stmt->execute();
+            
+            $result = $stmt->fetch();
+            
+            
+            return $result;
+        }
+        
+        function getProductosByNameOrDescript($nomb){
+            include './conexion.php';
+
+            $stmt = $conn->prepare("SELECT nombre, descripcion, foto, puntos, estado FROM producto WHERE nombre LIKE ? OR descripcion LIKE ?");
+            $stmt->bindValue(1,"%{$nomb}%", PDO::PARAM_STR);
+            $stmt->bindValue(2,"%{$nomb}%", PDO::PARAM_STR);
+            
+            $stmt->execute();
+            
+            $result = $stmt->fetchAll();
+            
+            return $result;
+        }
+        
+        function getProductos(){
+            
+            include('conexion.php');
+            
+            $sql = 'SELECT id_producto, nombre, descripcion, foto, puntos, estado FROM producto';
+            //$statmt = $conn->prepare($sql); con esta funcion no me funciona...
+            $result = $conn->query($sql);
+            // Extraer los valores de $result
+            $filas = $result->fetchAll();
+            
+            return $filas; // retornamos los valores
+            
+        }
+        
+        function insertar($nomb,$desc,$foto,$punt,$esta){
+           
+            // realizamos la conexion
+            include('conexion.php');
+
+            $ultimoId;
+            //ejecutamos el insert
+            //$statmt->execute();
+            try{
+                //preparamos la consulta insert
+                $statmt = $conn->prepare("INSERT INTO `producto`(`nombre`, `descripcion`, `foto`, `puntos`, `estado`) VALUES ('$nomb','$desc','$foto','$punt','$esta')");
+                try { 
+                    $conn->beginTransaction(); 
+                     //ejecutamos el insert
+                    $statmt->execute(); 
+                    $conn->commit();
+                    $stmt = $conn->query("SELECT LAST_INSERT_ID()"); // para poder retornar el ultimo id generado
+                    $lastId = $stmt->fetchColumn();
+                    $ultimoId = $lastId;
+                    //$ultimoId = $conn->lastInsertId(['id_producto']); // devolvemos el ultimo id insertado
+                }catch(PDOExecption $e) { 
+                    $conn->rollback(); 
+                    print "Error!: " . $e->getMessage() . "</br>"; 
+                } 
+            }catch( PDOExecption $e ) { 
+                print "Error!: " . $e->getMessage() . "</br>"; 
+            } 
+            echo $ultimoId;
+            /*}else{
+                echo '<script>';
+                echo 'console.log("No funca")';
+                echo '</script>';
+                
+                echo (filter_input_array($_POST));
+            }*/
+            
+            
+            // realizamos la desconexion de la BD
+            include('desconexion.php');
+        }
+                
+        //la funcion que sigue era de prueba
+        /*function guardarDatos(){
+            // incluimos el archivo de conexion con los datos y variables del mismo.
+            include('conexion.php');
+            //$servername = "localhost";$username = "root";$password = "";
+            //$dbname = "gaseosaarenba";
+            //$this->conectarBD();
+            //$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username,$password);
+            //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            // preparamos la consulta, falta pasar los valores por parametros
+            $statmt = $conn->prepare("INSERT INTO `producto`(`nombre`, `descripcion`, `foto`, `puntos`, `estado`) VALUES ('producto1','producto de prueba inicial','foto00001.jpg',15000,true)");
+            
+            //ejecutamos la consulta preparada.
+            $statmt->execute();
+
+            /*$resultado = $statmt->fetchAll();
+            foreach ($resultado as $row) {
+                echo $row["apellido"];
+            }*/
+        
+    }
+    
+    // instanciamos un objeto "Producto"
+    //$producto = new Producto();
+    
+    // llamamos al metodo INSERTAR
+    // hay que tomar los datos del campo del html de producto_agregar y pasarlos como parametros aca.
+    //$producto->insertar();
+    //$producto->cerrarConexionBD();
+    //$producto->getProductoByID('1');
+    
+?>
+
